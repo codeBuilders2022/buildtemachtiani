cb92f1ccf754aad889c2024ba9dfd7659e0f8146
codeBuilders2022 <codebuilders2022@gmail.com>
2023-04-17 11:28:01 -0600

    metics

--- src/components/atoms/CategoryMetric/CategoryMetric.jsx
+++ src/components/atoms/CategoryMetric/CategoryMetric.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './CategoryMetric.scss'
 
 
-const CategoryMetric = ({title,metric,id})=>
+const CategoryMetric = ({title,metric,id, style})=>
 {
     return(
         <>
-            <div className="CategoryMetric" id={id}>
+            <div className="CategoryMetric" id={id} style={style}>
                 <div className="title-category">{title}</div>
                 <div className="number-metric">{metric}</div>
             </div>
