1ac3535b4088b6d9cab6467b19e60e6dc30f3ca3
codeBuilders2022 <codebuilders2022@gmail.com>
2023-05-23 22:39:11 -0600

    sidebar

--- src/components/organisms/Sidebar/Sidebar.jsx
+++ src/components/organisms/Sidebar/Sidebar.jsx
@@ -118,7 +118,7 @@ const Sidebar = () => {
                                 <circle cx="12" cy="12" r="2" />
                                 <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
                             </svg> */}
-                            <label>Proximamente</label>
+                            <label>Próximamente</label>
                         </div>
                     </div>
                 </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -155,10 +155,10 @@ const Home = () => {
     }
 
     const dataMethric = [
-        { title: "Factor de impacto 2022", data: 1.22 },
-        { title: "Citescore 2022", data: 1.3 },
-        { title: "SJR 2022", data: 0.36 },
-        { title: "SNIP 2022", data: 0.67 }
+        { title: "Factor de impacto 2022", data: null },
+        { title: "Citescore 2022", data: null },
+        { title: "SJR 2022", data: null },
+        { title: "SNIP 2022", data: null }
     ]
     const [preVol, setPreVol] = useState([
         { title: "Volumen. 2. Num. 1", cover: cover, monthPlublished: "Octubre", pag: "222-289" },
@@ -243,7 +243,8 @@ const Home = () => {
                                     )
                                 })}
                             </div>
-                            <div className="icons">
+                            {/* --------------------------------- redes sociales ------------------------- */}
+                            {/* <div className="icons">
                                 <p style={{ marginRight: '10px' }}>Síguenos: </p>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#9e9e9e" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -253,7 +254,7 @@ const Home = () => {
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
                                 </svg>
-                            </div>
+                            </div> */}
                         </div>
                         <div className='right'>
                             {dataMethric.map((e, index) => {
@@ -263,7 +264,7 @@ const Home = () => {
                                         <div className='cardGraph'
                                             key={index}>
                                             <p>{e.title}:</p>
-                                            <p className="data">{e.data}</p>
+                                            <p className="data">{e.data !== null ? e.data : "-"}</p>
                                         </div>
                                     </>
                                 )
