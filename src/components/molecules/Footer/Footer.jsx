d99dcd11e08df1d4f5056fd2f9d23dad45232fdf
codeBuilders2022 <codebuilders2022@gmail.com>
2023-06-30 10:10:30 -0600

    footer

--- src/components/molecules/Footer/Footer.jsx
+++ src/components/molecules/Footer/Footer.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useStateContext } from "../../../contexts/ContextProvider";
 
+import "./Footer.scss"
+
 const Footer = () => {
   const { currentColor } = useStateContext();
 
   return (
-      <footer className="m-10 mt-24" id="footer_foo1">
-        <div
-          className="border-t-2 border-solid mb-5"
-          style={{ borderColor: currentColor }}
-        ></div>
-        <div className="md:flex md:justify-around">
+      <footer className="Footer_r" style={{borderColor: currentColor}} id="footer_foo1">
+        <section className="md:flex md:justify-around secct_1_m">
           <div className="dark:text-gray-200 text-gray-700 mr-3">
-            <h1 className="text-lg uppercase mb-5">
+            <h1 className="text-lg uppercase ">
               Revista internacional de Ciencias de la Educación Temachtiani
             </h1>
             <h1>ISSN-e (En proceso)</h1>
@@ -26,9 +24,17 @@ const Footer = () => {
                 educiencia@revistatemachtiani.net
               </a>
             </span>
-            <h1>San Lorenzo 381 Guadalajara - México</h1>
+            <h1>San Lorenzo 381 Guadalajara, Jalisco - México</h1>
           </div>
-        </div>
+        </section>
+        <section className="secc_bulders">
+          <p className="dark:text-gray-200 text-gray-700">
+            <strong>© 2022 Desarrollado por CodeBuilders</strong>
+          </p>
+          <div className="class_span dark:text-sky-200 text-sky-900">
+            <a href="https://businesscodebuilders.com/" target="_blank" rel="noreferrer">businesscodebuilders.com</a>
+          </div>
+        </section>
       </footer>
   );
 };
